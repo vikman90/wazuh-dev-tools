bcbcc2d1bfdba37498917303c2ca31df21d2ff07
sebasfalcone <sebastian.falcone@wazuh.com>
2022-07-19 09:04:54 -0300

    CL: - Added windows hostname->ip

--- shared/config.sh
+++ shared/config.sh
@@ -2,7 +2,7 @@
 # Falcone sebastian
 # July 17, 2022
 
-TIMEZONE="America/Argentina/Buenos_Aires"
+TIMEZONE="America/Buenos_Aires"
 
 GIT_NAME="sebasfalcone"
 GIT_EMAIL="sebastian.falconr@wazuh.com"
