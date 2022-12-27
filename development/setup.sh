f6e455e14b1da6adfb733c601e968ed688617e29
sebasfalcone <sebastian.falcone@wazuh.com>
2022-12-27 15:48:32 -0300

    CL: - Added docker support

--- development/setup.sh
+++ development/setup.sh
@@ -16,12 +16,16 @@ apt-get-upgrade() {
     DEBIAN_FRONTEND='noninteractive' apt-get upgrade -y
 }
 
+apt-get-update() {
+    apt-get update
+}
+
 apt-key-add() {
     APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add -
 }
 
 setup_packages() {
-    apt-get update
+    apt-get-update
 
     apt-get-install apt-transport-https
     apt-get-install nano
@@ -30,6 +34,8 @@ setup_packages() {
     apt-get-install git
     apt-get-install net-tools
     apt-get-install gnupg2
+    apt-get-install ca-certificates
+    apt-get-install lsb-release
 
     apt-get-install build-essential
     apt-get-install libssl-dev
@@ -43,11 +49,26 @@ setup_packages() {
     apt-get-install mysql-server
     apt-get-install postgresql-client
 
+    # VSCODE
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
     add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
     apt-get-install code  
 
+    # DOCKER
     apt-get-upgrade
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt-get-update
+    apt-get-install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+    sudo groupadd docker
+    sudo usermod -aG docker vagrant
+    newgrp docker
 }
 
 setup_wazuh_repo() {
