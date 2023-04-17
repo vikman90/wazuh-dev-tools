bfd25d81806cd53f8d3372d9b0575a6927bdf548
sebas <sebas@pop-os.localdomain>
2023-04-17 10:57:41 -0300

    CL: - Fixed shared files logic

--- vagrant/shared/config.sh
+++ vagrant/shared/config.sh
@@ -3,7 +3,3 @@
 # July 17, 2022
 
 TIMEZONE="America/Buenos_Aires"
-
-GIT_NAME="sebasfalcone"
-GIT_EMAIL="sebastian.falconr@wazuh.com"
-GIT_SIGNINGKEY="3092A1B894CF8BFA"
--- vagrant/shared/private/README.md
+++ vagrant/shared/private/README.md
@@ -4,3 +4,4 @@ You need to copy the following files here:
 - gpg_key.asc
 - id_rsa
 - id_rsa.pub
+- .gitconfig
