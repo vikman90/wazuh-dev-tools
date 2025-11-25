eb68bb52e2dd52673c5600768e5f581f8a8418b8
unknown <vmfdez90@gmail.com>
2025-11-25 12:53:50 +0100

    feat: add Debian 13 Trixie

--- vagrant/debian/setup.sh
+++ vagrant/debian/setup.sh
@@ -70,8 +70,8 @@ setup_cmocka_win() {
 }
 
 setup_wazuh_repo() {
-    curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key-add
-    echo "deb https://packages.wazuh.com/4.x/apt/ stable main" > /etc/apt/sources.list.d/wazuh.list
+    curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+    echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 }
 
 setup_nfs() {
